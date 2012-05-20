366224ac208023ad568e7444ed546a2aeb24f549
Christian Van Der Henst <info@maestrosdelweb.com>
2012-05-20 17:22:49 -0600

    ultimos cambios, faltan las imagenes de la pizza

--- codiguito.js
+++ codiguito.js
@@ -1,10 +1,80 @@
-var nodo;
+var nodo, papel, pizza;
 $(document).on("ready", iniciar);
 function iniciar()
 {
-	delete localStorage.nombre;
+	//delete localStorage.nombre;
 	transicion();
 	$("#formulario").on("submit", guardar);
+	//geolocalizar();
+}
+function aleatorio(min, max)
+{
+	return Math.floor(Math.random() * (min - max + 1) + max);
+}
+function dibujar()
+{
+	papel = Raphael("canvas", 200, 200);
+	pizza = papel.set();
+	pizza.push(
+		papel.image("base.png", 0, 0, 200, 200)
+	);
+}
+function geolocalizar()
+{
+	navigator.geolocation.getCurrentPosition(
+		mostrarOMapa,
+		errorMapa,
+		{
+			maximumAge: 100,
+			timeout: 10000
+		}
+	);
+}
+function mostrarOMapa(datos)
+{
+	var lat = datos.coords.latitude;
+	var lon = datos.coords.longitude;
+	$("#status").text("Ajá! Estás en: " + lat + "," + lon);
+	var map, layer;
+	map = new OpenLayers.Map( 'mapa_canvas');
+    layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+    map.addLayer(layer);
+    map.setCenter(
+        new OpenLayers.LonLat(lon, lat).transform(
+            new OpenLayers.Projection("EPSG:4326"),
+            map.getProjectionObject()
+        ), 18
+    );   
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+ 	var lonLat = new OpenLayers.LonLat( lon , lat );
+    markers.addMarker(new OpenLayers.Marker(lonLat));
+}
+function mostrarMapa(datos)
+{
+	var lat = datos.coords.latitude;
+	var lon = datos.coords.longitude;
+	$("#status").text("Ajá! Estás en: " + lat + "," + lon);
+	var coordenada = new google.maps.LatLng(lat, lon);
+	var opciones = {
+		center: coordenada,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	};
+	var mapa = new google.maps.Map(
+		$("#mapa_canvas")[0]
+		, opciones);
+	var opcionesChinche = {
+		position: coordenada,
+		map: mapa,
+		title: "Coordenadas de bombardeo"
+	};
+	var chinche = new google.maps.Marker(opcionesChinche);
+}
+function errorMapa(errorsh)
+{
+	$("#status").text("Tarde o temprano ¬_¬");
+	console.debug(errorsh);
 }
 function guardar(ev)
 {
@@ -20,10 +90,26 @@ function transicion()
 	{
 		$("#nombre").hide();
 		$("#enviar").css("display", "none");
-		$("#historia").slideUp();
+		$("#historia").slideUp(1000);
+		$("#pizzamaker").slideDown(1000);
 		$("#formulario label").text("Pura vida " + localStorage.nombre);
-		conectar(localStorage.nombre);
+		//conectar(localStorage.nombre);
+		$("#ingredientes article").on("click", agregarIngrediente);
+		dibujar();
 	}
+}
+function agregarIngrediente(ev)
+{
+	var porcion = ev.currentTarget.id;
+	var rotacion = aleatorio(0,360);
+	var tx = aleatorio(-30, 90);
+	var ty = aleatorio(-60, 60);
+	var proporcion = aleatorio(2, 7);
+	var trocito = papel.image(porcion + ".png", tx, ty, 150, 150);
+	trocito.scale(proporcion * 0.1, proporcion * 0.1);
+	trocito.rotate(rotacion);
+	pizza.push(trocito);
+	$("#pizza ul").append("<li>" + porcion + "</li>");
 }
 function conectar(nom)
 {
